33939d79a198891569ad6c1aaa76638de9b65c79
DargoDargonyx <dargodargonyx@outlook.com>
2025-03-24 15:57:15 -0400

    Edited ConversionPanel

---
+++ main/ConversionPanel.java
@@ -0,0 +1,41 @@
+import javax.swing.JPanel;
+
+/**
+ * This class is used to make the currency conversion portion
+ * of the calculator app.
+ * 
+ * @author DargoDargonyx
+ * @author Tencianity
+ * @version 03.24.25
+ */
+public class ConversionPanel {
+    
+    private static final int X_LOC = 100;
+    private static final int Y_LOC = 100;
+    private static final int MAIN_PANEL_HEIGHT = 800;
+    private static final int MAIN_PANEL_WIDTH = 600;
+    private JPanel mainPanel;
+    private JPanel inputPanel;
+    private JPanel outputPanel;
+
+    public ConversionPanel() {
+
+    }
+    private JPanel getMainPanel() {
+
+        return this.mainPanel;
+
+    }
+    private void initializeInnerPanels() {
+
+        
+
+    }
+    private void initializeInput() {
+
+    }
+    private void initializeOutput() {
+
+    }
+
+}
