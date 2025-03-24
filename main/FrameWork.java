cbe2f4f01b390d38543580a147a2d69a7939f6ee
DargoDargonyx <dargodargonyx@outlook.com>
2025-03-24 14:50:35 -0400

    Created the FrameWork class

---
+++ main/FrameWork.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+/**
+ * This class is meant to provide the JFrames in the calculator app
+ * that will be built. This includes any tabs needed for the GUI as
+ * well as controlling the placement of buttons and such.
+ * 
+ * @author DargoDargonyx
+ * @author ???
+ * @version 3/24/2025
+ * 
+ */
+public class FrameWork {
+
+}
