ea66c2d49af1a1d97ceaf94391b0386580574424
DargoDargonyx <dargodargonyx@outlook.com>
2025-03-24 15:42:52 -0400

    Created demo files

--- main/FrameConversion.java
+++ main/FrameConversion.java
@@ -1,3 +1,16 @@
+import javax.swing.JFrame;
+
+/**
+ * This class is used to make the currency conversion portion
+ * of the calculator app.
+ * 
+ * @author DargoDargonyx
+ * @author Tencianity
+ * @version 03.24.25
+ */
 public class FrameConversion {
     
+    private static final int X_LOC = 100;
+    private JFrame conversionFrame;
+
 }
